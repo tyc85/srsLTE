ef5712fd52c783b61479ad28681da7efbc03f8ab
Tsung-Yi Chen <steven.tychen@gmail.com>
2019-10-27 14:56:37 -0700

    ignore underflow warning for now

--- srsenb/src/enb.cc
+++ srsenb/src/enb.cc
@@ -372,7 +372,7 @@ void enb::handle_rf_msg(srslte_rf_error_t error)
   }else if(error.type == srslte_rf_error_t::SRSLTE_RF_ERROR_UNDERFLOW) {
     rf_metrics.rf_u++;
     rf_metrics.rf_error = true;
-    rf_log.warning("Underflow\n");
+    //rf_log.warning("Underflow\n");
   } else if(error.type == srslte_rf_error_t::SRSLTE_RF_ERROR_LATE) {
     rf_metrics.rf_l++;
     rf_metrics.rf_error = true;
